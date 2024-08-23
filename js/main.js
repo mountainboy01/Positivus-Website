98f95a4c6c43208a492b422081dc42adecb411a3
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-08-23 15:35:57 +0530

    add section

--- js/main.js
+++ js/main.js
@@ -101,3 +101,31 @@ toggleIcon.addEventListener("click",()=>{
       },
     });
 /************************CASE STUDIES SWIPER***************************/
+
+/************************OUR WORK ACCORDIAN***************************/
+ const accordianitems = document.querySelectorAll(".accordian-item");
+            accordianitems.forEach(item =>{
+                const title = item.querySelector(".accordian-title");
+                const content = item.querySelector(".accordian-content");
+                const icon = item.querySelector(".accordian-icon");
+
+                title.addEventListener("click" ,()=>{
+                    for(var i=0; i<accordianitems.length;i++)
+                        {
+                            if(accordianitems[i]!=item)
+                             {
+                                 accordianitems[i].classList.remove("active");
+                             }
+
+                            else
+                             {
+                                 item.classList.toggle("active");
+                             }
+                        }
+                });
+            });
+          
+
+
+/************************OUR WORK ACCORDIAN***************************/
+
